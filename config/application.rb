e98754a20b6a444adca6c7d3d0edb21e73610573
Amirol Ahmad <9w2pdf@gmail.com>
2016-06-29 12:41:27 +0800

    initial added

---
+++ config/application.rb
@@ -0,0 +1,27 @@
+require File.expand_path('../boot', __FILE__)
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Lbr
+  class Application < Rails::Application
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.time_zone = 'Asia/Kuala_Lumpur'
+    config.active_record.default_timezone = :local
+    config.active_record.time_zone_aware_attributes = false
+    config.encoding = "utf-8"
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  namespace :admin do
+  get 'dashboards/index'
+  end
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations',
+    omniauth: 'users/omniauth',
+    unlocks: 'users/unlocks'
+  }
+  devise_scope :user do
+    delete 'sign_out', :to => 'users/sessions#destroy'
+    get '/login' => 'users/sessions#new'
+    get '/register' => 'users/registrations#new'
+    get '/settings' => 'users/registrations#edit'
+    root 'users/sessions#new'
+  end
+
+  get "/dashboard" => 'dashboards#index'
+end
