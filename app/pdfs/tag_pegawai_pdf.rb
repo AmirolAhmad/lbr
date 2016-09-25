b3f0191e0ed92981c459e97eec004968bf2c96a2
Amirol Ahmad <9w2pdf@gmail.com>
2016-09-26 04:27:39 +0800

    add akreditasi card for user to view

---
+++ app/pdfs/tag_pegawai_pdf.rb
@@ -0,0 +1,23 @@
+class TagPegawaiPdf < Prawn::Document
+  require 'prawn/table'
+  require 'open-uri'
+
+  def initialize(team_official, view)
+    super(:page_size => "A5", :background => "#{Rails.root}/app/assets/images/tag_pegawai.jpg")
+    @team_official = team_official
+    @view = view
+    details
+  end
+
+  def details
+    defaultimage =  "#{Rails.root}/app/assets/images/default-avatar.png"
+    userimage = "#{@team_official.profile_picture_url}"
+    image open("#{@team_official.profile_picture? ? userimage : defaultimage}"), width: 133, height: 171, at: [-30, 481]
+
+    draw_text "#{@team_official.name.upcase}", at: [45, 284], :size => 18, :style => :bold
+    draw_text "#{@team_official.position.upcase}", at: [75, 239], :size => 18, :style => :bold
+    draw_text "#{@team_official.team.team_name.upcase}", at: [75, 202], :size => 18, :style => :bold
+    draw_text "#{@team_official.ic_number.upcase}", at: [175, 159], :size => 18, :style => :bold
+    draw_text "#{@team_official.ref_id.upcase}", at: [75, 118], :size => 18, :style => :bold
+  end
+end
---
+++ app/pdfs/tag_pemain_pdf.rb
@@ -0,0 +1,23 @@
+class TagPemainPdf < Prawn::Document
+  require 'prawn/table'
+  require 'open-uri'
+
+  def initialize(team_player, view)
+    super(:page_size => "A5", :background => "#{Rails.root}/app/assets/images/tag_pemain.jpg")
+    @team_player = team_player
+    @view = view
+    details
+  end
+
+  def details
+    defaultimage =  "#{Rails.root}/app/assets/images/default-avatar.png"
+    userimage = "#{@team_player.player_picture_url}"
+    image open("#{@team_player.player_picture? ? userimage : defaultimage}"), width: 133, height: 171, at: [-30, 480]
+
+    draw_text "#{@team_player.player_name.upcase}", at: [45, 261], :size => 18, :style => :bold
+    draw_text "#{@team_player.team.team_name.upcase}", at: [75, 212], :size => 18, :style => :bold
+    draw_text "#{@team_player.jersey_no}", at: [285, 189], :size => 81, :style => :bold
+    draw_text "#{@team_player.ic_number.upcase}", at: [175, 166], :size => 18, :style => :bold
+    draw_text "#{@team_player.ref_id.upcase}", at: [75, 122], :size => 18, :style => :bold
+  end
+end
