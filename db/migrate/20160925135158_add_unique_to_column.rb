bfa891e7b2fc360ac994b0ba546fa588a6f8cdda
Amirol Ahmad <9w2pdf@gmail.com>
2016-09-25 21:59:23 +0800

    add unique ic to player

--- db/migrate/20160925135158_add_unique_to_column.rb
+++ db/migrate/20160925135158_add_unique_to_column.rb
@@ -1,6 +1,5 @@
 class AddUniqueToColumn < ActiveRecord::Migration
   def change
-    add_index :team_officials, :ic_number, :unique => true
     add_index :team_players, :ic_number, :unique => true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921191302) do
+ActiveRecord::Schema.define(version: 20160925135158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20160921191302) do
     t.string   "ref_id"
   end
 
+  add_index "team_players", ["ic_number"], name: "index_team_players_on_ic_number", unique: true, using: :btree
   add_index "team_players", ["ref_id"], name: "index_team_players_on_ref_id", unique: true, using: :btree
   add_index "team_players", ["team_id"], name: "index_team_players_on_team_id", using: :btree
 
