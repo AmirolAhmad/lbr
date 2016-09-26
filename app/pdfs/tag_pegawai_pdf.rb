e1e8bd7d63226b2d08d6c03e38792b8af93177a2
Amirol Ahmad <9w2pdf@gmail.com>
2016-09-26 09:54:26 +0800

    change size

--- app/pdfs/tag_pegawai_pdf.rb
+++ app/pdfs/tag_pegawai_pdf.rb
@@ -3,7 +3,7 @@ class TagPegawaiPdf < Prawn::Document
   require 'open-uri'
 
   def initialize(team_official, view)
-    super(:page_size => "A5", :background => "#{Rails.root}/app/assets/images/tag_pegawai.jpg")
+    super(:page_size => "A6", :background => "#{Rails.root}/app/assets/images/tag_pegawai.jpg")
     @team_official = team_official
     @view = view
     details
@@ -12,12 +12,12 @@ class TagPegawaiPdf < Prawn::Document
   def details
     defaultimage =  "#{Rails.root}/app/assets/images/default-avatar.png"
     userimage = "#{@team_official.profile_picture_url}"
-    image open("#{@team_official.profile_picture? ? userimage : defaultimage}"), width: 133, height: 171, at: [-30, 481]
+    image open("#{@team_official.profile_picture? ? userimage : defaultimage}"), width: 95, height: 118, at: [-32, 325]
 
-    draw_text "#{@team_official.name.upcase}", at: [45, 284], :size => 18, :style => :bold
-    draw_text "#{@team_official.position.upcase}", at: [75, 239], :size => 18, :style => :bold
-    draw_text "#{@team_official.team.team_name.upcase}", at: [75, 202], :size => 18, :style => :bold
-    draw_text "#{@team_official.ic_number.upcase}", at: [175, 159], :size => 18, :style => :bold
-    draw_text "#{@team_official.ref_id}", at: [75, 118], :size => 18, :style => :bold
+    draw_text "#{@team_official.name.upcase}", at: [-25, 180], :size => 10, :style => :bold
+    draw_text "#{@team_official.position.upcase}", at: [-25, 149], :size => 10, :style => :bold
+    draw_text "#{@team_official.team.team_name.upcase}", at: [-25, 123], :size => 10, :style => :bold
+    draw_text "#{@team_official.ic_number.upcase}", at: [-25, 92], :size => 10, :style => :bold
+    draw_text "#{@team_official.ref_id}", at: [-25, 63], :size => 10, :style => :bold
   end
 end
--- app/pdfs/tag_pemain_pdf.rb
+++ app/pdfs/tag_pemain_pdf.rb
@@ -3,7 +3,7 @@ class TagPemainPdf < Prawn::Document
   require 'open-uri'
 
   def initialize(team_player, view)
-    super(:page_size => "A5", :background => "#{Rails.root}/app/assets/images/tag_pemain.jpg")
+    super(:page_size => "A6", :background => "#{Rails.root}/app/assets/images/tag_pemain.jpg")
     @team_player = team_player
     @view = view
     details
@@ -12,12 +12,12 @@ class TagPemainPdf < Prawn::Document
   def details
     defaultimage =  "#{Rails.root}/app/assets/images/default-avatar.png"
     userimage = "#{@team_player.player_picture_url}"
-    image open("#{@team_player.player_picture? ? userimage : defaultimage}"), width: 133, height: 171, at: [-30, 480]
+    image open("#{@team_player.player_picture? ? userimage : defaultimage}"), width: 95, height: 118, at: [-32, 325]
 
-    draw_text "#{@team_player.player_name.upcase}", at: [45, 261], :size => 18, :style => :bold
-    draw_text "#{@team_player.team.team_name.upcase}", at: [75, 212], :size => 18, :style => :bold
-    draw_text "#{@team_player.jersey_no}", at: [285, 189], :size => 81, :style => :bold
-    draw_text "#{@team_player.ic_number.upcase}", at: [175, 166], :size => 18, :style => :bold
-    draw_text "#{@team_player.ref_id}", at: [75, 122], :size => 18, :style => :bold
+    draw_text "#{@team_player.player_name.upcase}", at: [-25, 161], :size => 10, :style => :bold
+    draw_text "#{@team_player.team.team_name.upcase}", at: [-25, 127], :size => 10, :style => :bold
+    draw_text "#{@team_player.jersey_no}", at: [200, 120], :size => 71, :style => :bold
+    draw_text "#{@team_player.ic_number.upcase}", at: [-25, 96], :size => 10, :style => :bold
+    draw_text "#{@team_player.ref_id}", at: [-25, 65], :size => 10, :style => :bold
   end
 end
