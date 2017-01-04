71dc9e742b6c0ceba5774bb5f73004d810cc74c3
Amirol Ahmad <9w2pdf@gmail.com>
2017-01-05 05:32:49 +0800

    delete button for match records

--- app/controllers/staff/match_reports_controller.rb
+++ app/controllers/staff/match_reports_controller.rb
@@ -222,9 +222,50 @@ class Staff::MatchReportsController < StaffController
   end
 
   def destroy
+    @group = Staff::Group.find params[:group_id]
+    @staff_team_schedule = Staff::TeamSchedule.find params[:team_schedule_id]
     @staff_match_report = Staff::MatchReport.find params[:id]
+    @siapa1 = Staff::GroupTeam.find_by_team_id (@staff_team_schedule.home_team_id)
+    @siapa2 = Staff::GroupTeam.find_by_team_id (@staff_team_schedule.away_team_id)
+
+    current_home_gm = @siapa1.gol_masuk
+    current_home_gb = @siapa1.gol_bolos
+    current_away_gm = @siapa2.gol_masuk
+    current_away_gb = @siapa2.gol_bolos
+    perlawanan_home = @siapa1.perlawanan
+    perlawanan_away = @siapa2.perlawanan
+    menang_home = @siapa1.menang
+    menang_away = @siapa2.menang
+    kalah_home = @siapa1.kalah
+    kalah_away = @siapa2.kalah
+    seri_home = @siapa1.seri
+    seri_away = @siapa2.seri
+    mata_home = @siapa1.mata
+    mata_away = @siapa2.mata
+
+    @siapa1.update(perlawanan: perlawanan_home -= 1)
+    @siapa2.update(perlawanan: perlawanan_away -= 1)
+    @siapa1.update(gol_masuk: current_home_gm -= @staff_match_report.score_home_team_was)
+    @siapa1.update(gol_bolos: current_home_gb -= @staff_match_report.score_away_team_was)
+    @siapa2.update(gol_masuk: current_away_gm -= @staff_match_report.score_away_team_was)
+    @siapa2.update(gol_bolos: current_away_gb -= @staff_match_report.score_home_team_was)
+
+    if @staff_match_report.score_home_team_was > @staff_match_report.score_away_team_was
+      @siapa1.update(menang: menang_home -= 1)
+      @siapa2.update(kalah: kalah_away -= 1)
+      @siapa1.update(mata: mata_home -= 3)
+    elsif @staff_match_report.score_home_team_was < @staff_match_report.score_away_team_was
+      @siapa1.update(kalah: kalah_home -= 1)
+      @siapa2.update(menang: menang_away -= 1)
+      @siapa2.update(mata: mata_away -= 3)
+    elsif @staff_match_report.score_home_team_was == @staff_match_report.score_away_team_was
+      @siapa1.update(seri: seri_home -= 1)
+      @siapa2.update(seri: seri_away -= 1)
+      @siapa1.update(mata: mata_home -= 1)
+      @siapa2.update(mata: mata_away -= 1)
+    end
     if @staff_match_report.destroy
-      redirect_to staff_zone_group_team_schedule_match_reports_path, notice: 'Berjaya dihapuskan!'
+      redirect_to staff_zone_group_team_schedule_path(id:@staff_team_schedule), notice: 'Berjaya dihapuskan!'
     else
       render 'index'
     end
